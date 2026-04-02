dc6b4d7d2840adfb47997aec03e1fc7a1ef33102
UCC <ucc@example.com>
2026-04-02 20:55:34 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print('Name: UCC')
 print('Date: 2026-04-02')
 
 print('5 + 3 =', add(5, 3))
 print('5 - 3 =', subtract(5, 3))
+print('5 * 3 =', multiply(5, 3))
